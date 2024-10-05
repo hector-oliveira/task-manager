4e15c3a272384124dcdb2b754f19096883305852
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-05 18:04:45 -0300

    feat: FormToAddTask new component

--- src/components/form-to-add-task.tsx
+++ src/components/form-to-add-task.tsx
@@ -1,8 +1,51 @@
-function FormToAddTask() {
+import { useState } from "react";
+
+interface FormToAddTaskProps {
+  onAdd: (title: string, description: string) => void;
+}
+
+function FormToAddTask({ onAdd }: FormToAddTaskProps) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  function handleForm() {
+    if (!title.trim() || !description.trim()) {
+      alert("Preencha todos os campos");
+      setTitle("");
+      setDescription("");
+      return;
+    }
+    onAdd(title, description);
+    setTitle("");
+    setDescription("");
+  }
   return (
-    <>
-      <h1>FormToAddTask Page</h1>
-    </>
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow ring-1 ring-slate-300 flex flex-col">
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Adicionar Tarefa"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md ring-1 ring-slate-300"
+      />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Descrição"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md ring-1 ring-slate-300"
+      />
+      <button
+        onClick={() => handleForm()}
+        className="bg-slate-500 p-2 rounded-md text-white hover:bg-slate-600"
+      >
+        Adicionar
+      </button>
+    </div>
   );
 }
 
