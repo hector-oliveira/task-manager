48d46e0088a45f10163ebb38a02016412dc42997
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-05 14:54:14 -0300

    feat: function created - onTaskDelete

--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -1,25 +1,33 @@
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, TrashIcon } from "lucide-react";
 import { TaskListProps } from "../App";
 
 interface TaskListComponentProps {
   tasks: TaskListProps[];
-  callFunction: (taskId: number) => void;
+  onUpdate: (taskId: number) => void;
+  onDelete: (taskId: number) => void;
 }
-function TaskList({ tasks, callFunction }: TaskListComponentProps) {
+
+function TaskList({ tasks, onUpdate, onDelete }: TaskListComponentProps) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow ring-1 ring-slate-300">
       {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
-            onClick={() => callFunction(task.id)}
-            className={`bg-slate-400 text-white text-left p-2 rounded-md w-full ${
+            onClick={() => onUpdate(task.id)}
+            className={`bg-slate-400 text-white text-left p-2 rounded-md w-full hover:bg-slate-500 ${
               task.isCompleted && "line-through"
             }`}
           >
             {task.title}
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button className="bg-slate-400 p-2 rounded-md text-white hover:bg-slate-500">
             <ChevronRight />
+          </button>
+          <button
+            onClick={() => onDelete(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white hover:bg-slate-500"
+          >
+            <TrashIcon />
           </button>
         </li>
       ))}
