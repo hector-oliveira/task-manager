949fa6f918eb98c8ca5a63f07328b32aaa00695a
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-05 23:51:52 -0300

    feat: navigate to TaskDetails screens

--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -1,5 +1,6 @@
 import { ChevronRight, TrashIcon } from "lucide-react";
 import { TaskListProps } from "../App";
+import { useNavigate } from "react-router-dom";
 
 interface TaskListComponentProps {
   tasks: TaskListProps[];
@@ -8,6 +9,13 @@ interface TaskListComponentProps {
 }
 
 function TaskList({ tasks, onUpdate, onDelete }: TaskListComponentProps) {
+  const navigate = useNavigate();
+  function handleSeeTaskDetails({ title, description }: TaskListProps) {
+    const queryParams = new URLSearchParams();
+    queryParams.set("title", title);
+    queryParams.set("description", description);
+    navigate(`details?${queryParams.toString()}`);
+  }
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow ring-1 ring-slate-300">
       {tasks.map((task) => (
@@ -20,7 +28,10 @@ function TaskList({ tasks, onUpdate, onDelete }: TaskListComponentProps) {
           >
             {task.title}
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white hover:bg-slate-500">
+          <button
+            onClick={() => handleSeeTaskDetails(task)}
+            className="bg-slate-400 p-2 rounded-md text-white hover:bg-slate-500"
+          >
             <ChevronRight />
           </button>
           <button
