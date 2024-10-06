2acb9d56b062932e9ab2b7176451f2d4f3ab6cd5
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-05 22:40:14 -0300

    feat: TaskDetails screen

---
+++ src/pages/task-details/TaskDetails.tsx
@@ -0,0 +1,11 @@
+import Container from "../../components/container";
+
+function TaskDetails() {
+  return (
+    <Container>
+      <h1>TaskDetails Page</h1>
+    </Container>
+  );
+}
+
+export default TaskDetails;
