be7965c5513be04f3bdd8fcd321599be31eeac8d
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-07 16:19:47 -0300

    refactor: renderTaskList

---
+++ src/helpers/render-task-list.tsx
@@ -0,0 +1,40 @@
+import { ChevronRight, TrashIcon } from "lucide-react";
+import { TaskListProps } from "../App";
+import Button from "../components/Button";
+import { TaskListComponentProps } from "../components/task-list";
+
+interface RenderTaskList extends TaskListComponentProps {
+  handleSeeTaskDetails: ({ title, description }: TaskListProps) => void;
+}
+
+function renderTasks({
+  handleSeeTaskDetails,
+  onUpdate,
+  onDelete,
+  tasks,
+}: RenderTaskList) {
+  return (
+    <>
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <Button
+            onClick={() => onUpdate(task.id)}
+            className={`w-full hover:bg-slate-500 ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </Button>
+          <Button onClick={() => handleSeeTaskDetails(task)}>
+            <ChevronRight />
+          </Button>
+          <Button onClick={() => onDelete(task.id)}>
+            <TrashIcon />
+          </Button>
+        </li>
+      ))}
+    </>
+  );
+}
+
+export { renderTasks };
