d21df874f065d0df78275b32d07d326b40f9759e
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-07 16:15:06 -0300

    refactor: FormToAddTask component

--- src/components/form-to-add-task.tsx
+++ src/components/form-to-add-task.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Input from "./input";
 
 interface FormToAddTaskProps {
   onAdd: (title: string, description: string) => void;
@@ -21,23 +22,17 @@ function FormToAddTask({ onAdd }: FormToAddTaskProps) {
   }
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow ring-1 ring-slate-300 flex flex-col">
-      <input
+      <Input
         type="text"
-        name=""
-        id=""
-        placeholder="Adicionar Tarefa"
+        placeholder="Escreva uma tarefa"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
-        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md ring-1 ring-slate-300"
       />
-      <input
+      <Input
         type="text"
-        name=""
-        id=""
-        placeholder="Descrição"
+        placeholder="Faça uma descrição"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
-        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md ring-1 ring-slate-300"
       />
       <button
         onClick={() => handleForm()}
