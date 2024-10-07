a1e220c89792e507555b9764ef120cf0804b0e11
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-07 16:17:11 -0300

    feat: Button new component

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+function Button({ children, ...rest }: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      className={`bg-slate-400 text-white text-left p-2 rounded-md ${rest.className}`}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
