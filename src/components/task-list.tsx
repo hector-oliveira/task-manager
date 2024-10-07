52f2540765239ece65166842988d397e59fe34ff
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-07 16:16:16 -0300

    refactor: TaskList component with renderTaks function

--- src/components/task-list.tsx
+++ src/components/task-list.tsx
@@ -1,47 +1,18 @@
-import { ChevronRight, TrashIcon } from "lucide-react";
 import { TaskListProps } from "../App";
-import { useNavigate } from "react-router-dom";
+import { renderTasks } from "../helpers/render-task-list";
+import { useTaskList } from "../hooks/use-task-list";
 
-interface TaskListComponentProps {
+export interface TaskListComponentProps {
   tasks: TaskListProps[];
   onUpdate: (taskId: string) => void;
   onDelete: (taskId: string) => void;
 }
 
 function TaskList({ tasks, onUpdate, onDelete }: TaskListComponentProps) {
-  const navigate = useNavigate();
-  function handleSeeTaskDetails({ title, description }: TaskListProps) {
-    const queryParams = new URLSearchParams();
-    queryParams.set("title", title);
-    queryParams.set("description", description);
-    navigate(`details?${queryParams.toString()}`);
-  }
+  const { handleSeeTaskDetails } = useTaskList();
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow ring-1 ring-slate-300">
-      {tasks.map((task) => (
-        <li key={task.id} className="flex gap-2">
-          <button
-            onClick={() => onUpdate(task.id)}
-            className={`bg-slate-400 text-white text-left p-2 rounded-md w-full hover:bg-slate-500 ${
-              task.isCompleted && "line-through"
-            }`}
-          >
-            {task.title}
-          </button>
-          <button
-            onClick={() => handleSeeTaskDetails(task)}
-            className="bg-slate-400 p-2 rounded-md text-white hover:bg-slate-500"
-          >
-            <ChevronRight />
-          </button>
-          <button
-            onClick={() => onDelete(task.id)}
-            className="bg-slate-400 p-2 rounded-md text-white hover:bg-slate-500"
-          >
-            <TrashIcon />
-          </button>
-        </li>
-      ))}
+      {renderTasks({ handleSeeTaskDetails, onDelete, onUpdate, tasks })}
     </ul>
   );
 }
