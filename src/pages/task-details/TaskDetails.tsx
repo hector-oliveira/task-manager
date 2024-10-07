bfcc7e223384d959482bf3002bf669ee28c8a131
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-07 12:05:04 -0300

    feat: added styles to the Task Details page and added the back page function

--- src/pages/task-details/TaskDetails.tsx
+++ src/pages/task-details/TaskDetails.tsx
@@ -1,14 +1,28 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import Container from "../../components/container";
+import { ChevronLeftIcon } from "lucide-react";
 
 function TaskDetails() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
     <Container>
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <div className="w-[500px] space-y-4">
+        <div className="flex items-center mb-5">
+          <button onClick={() => navigate(-1)} className="text-slate-100">
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl font-bold text-slate-100 flex-1 text-center">
+            Detalhes da Tarefas
+          </h1>
+        </div>
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-2xl font-bold text-slate-600">{title}</h2>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
     </Container>
   );
 }
