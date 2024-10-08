368be63dc31e6d6b2a8615fb25d81db890db4e42
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-08 01:36:30 -0300

    feat: Input components

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,11 +1,29 @@
-import { InputHTMLAttributes } from "react";
+import { LucideProps } from "lucide-react";
+import React from "react";
+import { InputHTMLAttributes, useState } from "react";
 
-function Input({ ...rest }: InputHTMLAttributes<HTMLInputElement>) {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+}
+
+function Input({ icon, ...rest }: InputProps) {
+  const [isFocused, setIsFocused] = useState(false);
   return (
-    <input
-      {...rest}
-      className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md ring-1 ring-slate-300"
-    />
+    <section
+      className={`flex items-center pl-2 gap-2 border border-slate-300 outline-slate-400 rounded-md ring-1 ring-slate-300 ${
+        isFocused ? "ring-blue-500 border border-blue-500" : ""
+      }`}
+    >
+      {icon && React.createElement(icon)}
+      <input
+        {...rest}
+        className="flex-1 outline-none p-3 bg-slate-200"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+    </section>
   );
 }
 
