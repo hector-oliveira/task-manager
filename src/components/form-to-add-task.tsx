1d97ec7e99a1d96d126bd25478e913f38b49bde0
Hector Oliveira <hectorabreu_oliveira@hotmail.com>
2024-10-08 01:35:22 -0300

    improve: FormToAddTask components

--- src/components/form-to-add-task.tsx
+++ src/components/form-to-add-task.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "./input";
+import { ClipboardList, PencilIcon } from "lucide-react";
 
 interface FormToAddTaskProps {
   onAdd: (title: string, description: string) => void;
@@ -9,7 +10,8 @@ function FormToAddTask({ onAdd }: FormToAddTaskProps) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  function handleForm() {
+  function handleForm(event?: React.FormEvent<HTMLFormElement>) {
+    event?.preventDefault();
     if (!title.trim() || !description.trim()) {
       alert("Preencha todos os campos");
       setTitle("");
@@ -21,26 +23,31 @@ function FormToAddTask({ onAdd }: FormToAddTaskProps) {
     setDescription("");
   }
   return (
-    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow ring-1 ring-slate-300 flex flex-col">
+    <form
+      onSubmit={handleForm}
+      className="space-y-4 p-6 bg-slate-200 rounded-md shadow ring-1 ring-slate-300 flex flex-col"
+    >
       <Input
         type="text"
-        placeholder="Escreva uma tarefa"
+        placeholder="Adicione uma tarefa"
         value={title}
+        icon={ClipboardList}
         onChange={(e) => setTitle(e.target.value)}
       />
       <Input
         type="text"
         placeholder="Faça uma descrição"
+        icon={PencilIcon}
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
       <button
-        onClick={() => handleForm()}
+        type="submit"
         className="bg-slate-500 p-2 rounded-md text-white hover:bg-slate-600"
       >
         Adicionar
       </button>
-    </div>
+    </form>
   );
 }
 
